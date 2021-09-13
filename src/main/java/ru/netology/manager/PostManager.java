a6ec7f00797c96c85d958e6e30e9b47556ffd2ec
AAB-87 <andrej-abrekov@yandex.ru>
2021-09-13 19:26:39 +0400

    Added methods in PostManager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -9,4 +9,45 @@ public class PostManager {
         // TODO: add logic
         return null;
     }
+
+//    Wall.search
+
+    public Post[] getOwnerId() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getDomain() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getQuery() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getOwnersOnly() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getCount() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getOffset() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getExtended() {
+        // TODO: add logic
+        return null;
+    }
+    public Post[] getFields() {
+        // TODO: add logic
+        return null;
+    }
+
+//    Wall.delete
+
+    public void ownerId() {}
+    public void postId()  {}
+
 }
