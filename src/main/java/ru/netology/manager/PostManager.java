3d5687d344bf6c17e39d80d8aa8e64b8a237b326
AAB-87 <andrej-abrekov@yandex.ru>
2021-09-17 12:19:27 +0400

    fixed PostManager.java

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -5,49 +5,12 @@ import ru.netology.domain.Post;
 public class PostManager {
     private Post[] posts;
 
-    public Post[] getPostsForFeed() {
-        // TODO: add logic
+//    public Post[] getPostsForFeed() {
+//        // TODO: add logic
+//        return null;
+//    }
+    public Post[] search(int ownerId, String domain, String  query, boolean ownersOnly, int count, int offset, boolean extended, String fields) {
         return null;
     }
-
-//    Wall.search
-
-    public Post[] getOwnerId() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getDomain() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getQuery() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getOwnersOnly() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getCount() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getOffset() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getExtended() {
-        // TODO: add logic
-        return null;
-    }
-    public Post[] getFields() {
-        // TODO: add logic
-        return null;
-    }
-
-//    Wall.delete
-
-    public void ownerId() {}
-    public void postId()  {}
-
+    public void delete(int ownerId, int postId) {}
 }
